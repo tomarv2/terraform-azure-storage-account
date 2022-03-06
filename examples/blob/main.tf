4306801b711007eb30233bb9cd7f5102931ce5f9
tomarv2 <varuntomar2019@gmail.com>
2022-03-05 23:45:14 -0800

    initial commit

--- examples/blob/main.tf
+++ examples/blob/main.tf
@@ -18,8 +18,4 @@ module "blob" {
   storage_container_name = "test2"
   blob_name              = "test2-blob"
   blob_source            = "/tmp/<sample_file>"
-  #-----------------------------------------------
-  # Note: Do not change teamid and prjid once set.
-  teamid = var.teamid
-  prjid  = var.prjid
 }
